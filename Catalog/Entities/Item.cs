5f3662069c0f0b75abb27d525d3a83d9e96d53b9
mamapam <mamapam.dev@gmail.com>
2020-12-29 17:50:55 -0500

    Setup entity, repository and controller

---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Catalog.Entities
+{
+	public record Item
+	{
+		public Guid Id { get; init; }
+
+		public string Name { get; set; }
+
+		public decimal Price { get; set; }
+
+		public DateTimeOffset CreatedDate { get; init; }
+	}
+}
